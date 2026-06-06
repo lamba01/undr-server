9326d2622e35973a648652d5edbbee03af631f3b
lamba01 <jamesspaul987@gmail.com>
2026-06-06 13:59:15 +0100

    add: database migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260606_125832 from './20260606_125832';
+
+export const migrations = [
+  {
+    up: migration_20260606_125832.up,
+    down: migration_20260606_125832.down,
+    name: '20260606_125832'
+  },
+];
